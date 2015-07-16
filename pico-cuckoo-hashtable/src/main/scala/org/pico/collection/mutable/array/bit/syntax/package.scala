19b855ebcd76841d60a5283275a70effa5fbc3d2
John Ky <jky@arbor.net>
2015-07-16 20:09:37 +1000

    Implement for ints.

--- pico-cuckoo-hashtable/src/main/scala/org/pico/collection/mutable/array/bit/syntax/package.scala
+++ pico-cuckoo-hashtable/src/main/scala/org/pico/collection/mutable/array/bit/syntax/package.scala
@@ -6,9 +6,9 @@ package object syntax {
 
     final def getAtIndex(i: Long): Long = array(i.toInt)
 
-    final def bit(n: Long, v: Put[Long]): Unit = {
-      val i: Long = n / 64
-      val o: Long = n % 64
+    final def long(n: Long, v: Put[Long]): Unit = {
+      val i: Int = (n / 64).toInt
+      val o: Int = (n % 64).toInt
 
       if (o == 0) {
         setAtIndex(i, v.value)
@@ -20,14 +20,47 @@ package object syntax {
       }
     }
 
-    final def bit(n: Long): Long = {
-      val i: Long = n / 64
-      val o: Long = n % 64
+    final def long(n: Long): Long = {
+      val i: Int = (n / 64).toInt
+      val o: Int = (n % 64).toInt
 
       if (o == 0) {
         getAtIndex(i)
       } else {
         val p = 64 - o
+
+        getAtIndex(i + 0) << o | getAtIndex(i + 1) >>> p
+      }
+    }
+  }
+
+  implicit class IntBitArrayOps_2s8EdpV(val array: Array[Int]) extends AnyVal {
+    final def setAtIndex(i: Long, v: Int): Unit = array(i.toInt) = v
+
+    final def getAtIndex(i: Long): Int = array(i.toInt)
+
+    final def int(n: Long, v: Put[Int]): Unit = {
+      val i: Int = (n / 32).toInt
+      val o: Int = (n % 32).toInt
+
+      if (o == 0) {
+        setAtIndex(i, v.value)
+      } else {
+        val p = 32 - o
+
+        setAtIndex(i + 0, getAtIndex(i + 0) >>> p << p | (v.value >>> o))
+        setAtIndex(i + 1, getAtIndex(i + 1) << o >>> o | (v.value <<  p))
+      }
+    }
+
+    final def int(n: Long): Int = {
+      val i: Int = (n / 32).toInt
+      val o: Int = (n % 32).toInt
+
+      if (o == 0) {
+        getAtIndex(i)
+      } else {
+        val p: Int = (32 - o)
 
         getAtIndex(i + 0) << o | getAtIndex(i + 1) >>> p
       }
--- pico-cuckoo-hashtable/src/test/scala/org/pico/collection/mutable/array/bit/syntax/IntBitArraySpec.scala
+++ pico-cuckoo-hashtable/src/test/scala/org/pico/collection/mutable/array/bit/syntax/IntBitArraySpec.scala
@@ -4,16 +4,16 @@ import org.scalacheck.{Arbitrary, Gen}
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
-class LongBitArraySpec extends Specification with ScalaCheck {
+class IntBitArraySpec extends Specification with ScalaCheck {
   case class Offset(value: Long)
 
   implicit val arbitraryOffset = Arbitrary[Offset](Gen.choose(0L, 128L).map(Offset))
 
   "Values that are set can be retrieved again" in {
-    prop { (v: Long, offset: Offset) =>
-      val buffer = new Array[Long](5)
-      buffer.long(offset.value, Put(v))
-      buffer.long(offset.value) ==== v
+    prop { (v: Int, offset: Offset) =>
+      val buffer = new Array[Int](10)
+      buffer.int(offset.value, Put(v))
+      buffer.int(offset.value) ==== v
     }
   }
 }
