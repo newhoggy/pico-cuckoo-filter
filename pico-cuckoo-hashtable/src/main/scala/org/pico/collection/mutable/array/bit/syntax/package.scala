bf3180bcc99737ff2870ea96c1af7ad27fb743f6
John Ky <jky@arbor.net>
2015-07-16 20:31:42 +1000

    Operators to encourage consistency.

--- pico-cuckoo-hashtable/src/main/scala/org/pico/collection/mutable/array/bit/syntax/package.scala
+++ pico-cuckoo-hashtable/src/main/scala/org/pico/collection/mutable/array/bit/syntax/package.scala
@@ -1,35 +1,59 @@
 package org.pico.collection.mutable.array.bit
 
 package object syntax {
+  implicit class LongOps_2s8EdpV(val self: Long) extends AnyVal {
+    def >>>>(offset: Long): Long = self >>> offset
+
+    def <<<<(offset: Long): Long = self << offset
+
+    def ||||(that: Long): Long = self | that
+  }
+
+  implicit class IntOps_2s8EdpV(val self: Int) extends AnyVal {
+    def >>>>(offset: Long): Int = self >>> offset.toInt
+
+    def <<<<(offset: Long): Int = self << offset.toInt
+
+    def ||||(that: Int): Int = self | that
+  }
+
+  implicit class ShortOps_2s8EdpV(val self: Short) extends AnyVal {
+    def >>>>(offset: Long): Short = ((0xffff & self.toInt) >>> offset.toInt).toShort
+
+    def <<<<(offset: Long): Short = ((0xffff & self.toInt) << offset.toInt).toShort
+
+    def ||||(that: Short): Short = (self | that).toShort
+  }
+
   implicit class LongBitArrayOps_2s8EdpV(val array: Array[Long]) extends AnyVal {
     final def setAtIndex(i: Long, v: Long): Unit = array(i.toInt) = v
 
     final def getAtIndex(i: Long): Long = array(i.toInt)
 
     final def long(n: Long, v: Put[Long]): Unit = {
-      val i: Int = (n / 64).toInt
-      val o: Int = (n % 64).toInt
+      val i = n / 64
+      val o = n % 64
 
       if (o == 0) {
         setAtIndex(i, v.value)
       } else {
         val p = 64 - o
 
-        setAtIndex(i + 0, getAtIndex(i + 0) >>> p << p | (v.value >>> o))
-        setAtIndex(i + 1, getAtIndex(i + 1) << o >>> o | (v.value <<  p))
+        setAtIndex(i + 0, getAtIndex(i + 0) >>>> p <<<< p | (v.value >>>> o))
+        setAtIndex(i + 1, getAtIndex(i + 1) <<<< o >>>> o | (v.value <<<< p))
       }
     }
 
     final def long(n: Long): Long = {
-      val i: Int = (n / 64).toInt
-      val o: Int = (n % 64).toInt
+      val i = n / 64
+      val o = n % 64
 
       if (o == 0) {
         getAtIndex(i)
       } else {
         val p = 64 - o
 
-        getAtIndex(i + 0) << o | getAtIndex(i + 1) >>> p
+        getAtIndex(i + 0) << o |||| getAtIndex(i + 1) >>> p
       }
     }
   }
@@ -40,29 +64,29 @@ package object syntax {
     final def getAtIndex(i: Long): Int = array(i.toInt)
 
     final def int(n: Long, v: Put[Int]): Unit = {
-      val i: Int = (n / 32).toInt
-      val o: Int = (n % 32).toInt
+      val i = n / 32
+      val o = n % 32
 
       if (o == 0) {
         setAtIndex(i, v.value)
       } else {
         val p = 32 - o
 
-        setAtIndex(i + 0, getAtIndex(i + 0) >>> p << p | (v.value >>> o))
-        setAtIndex(i + 1, getAtIndex(i + 1) << o >>> o | (v.value <<  p))
+        setAtIndex(i + 0, getAtIndex(i + 0) >>>> p <<<< p | (v.value >>>> o))
+        setAtIndex(i + 1, getAtIndex(i + 1) <<<< o >>>> o | (v.value <<<< p))
       }
     }
 
     final def int(n: Long): Int = {
-      val i: Int = (n / 32).toInt
-      val o: Int = (n % 32).toInt
+      val i = n / 32
+      val o = n % 32
 
       if (o == 0) {
         getAtIndex(i)
       } else {
-        val p: Int = 32 - o
+        val p = 32 - o
 
-        getAtIndex(i + 0) << o | getAtIndex(i + 1) >>> p
+        getAtIndex(i + 0) <<<< o |||| getAtIndex(i + 1) >>>> p
       }
     }
   }
@@ -73,29 +97,29 @@ package object syntax {
     final def getAtIndex(i: Long): Short = array(i.toInt)
 
     final def short(n: Long, v: Put[Short]): Unit = {
-      val i: Int = (n / 16).toInt
-      val o: Int = (n % 16).toInt
+      val i = n / 16
+      val o = n % 16
 
       if (o == 0) {
         setAtIndex(i, v.value)
       } else {
         val p = 16 - o
 
-        setAtIndex(i + 0, (getAtIndex(i + 0) >>> p << p | (v.value >>> o)).toShort)
-        setAtIndex(i + 1, (getAtIndex(i + 1) << o >>> o | (v.value <<  p)).toShort)
+        setAtIndex(i + 0, (getAtIndex(i + 0) >>>> p <<<< p | (v.value >>>> o)).toShort)
+        setAtIndex(i + 1, (getAtIndex(i + 1) <<<< o >>>> o | (v.value <<<< p)).toShort)
       }
     }
 
     final def short(n: Long): Short = {
-      val i: Int = (n / 16).toInt
-      val o: Int = (n % 16).toInt
+      val i = n / 16
+      val o = n % 16
 
       if (o == 0) {
         getAtIndex(i)
       } else {
-        val p: Int = 16 - o
+        val p = 16 - o
 
-        ((0xffff & getAtIndex(i + 0).toInt) << o | (0xffff & getAtIndex(i + 1).toInt) >>> p).toShort
+        getAtIndex(i + 0) <<<< o |||| getAtIndex(i + 1) >>>> p
       }
     }
   }
