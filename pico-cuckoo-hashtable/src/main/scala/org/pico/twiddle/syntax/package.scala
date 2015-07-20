1b8501b900bd9a29a980c567cb264b229174b862
John Ky <jky@arbor.net>
2015-07-20 23:06:34 +1000

    Simplify.

--- pico-cuckoo-hashtable/src/main/scala/org/pico/twiddle/syntax/package.scala
+++ pico-cuckoo-hashtable/src/main/scala/org/pico/twiddle/syntax/package.scala
@@ -6,8 +6,8 @@ package object syntax {
 
     def uint: Int = 0xff & self.toInt
 
-    def >>>>(offset: Long): Byte = if (offset > 0) (uint >>> offset.toInt).toByte else (uint <<  -offset.toInt).toByte
-    def <<<<(offset: Long): Byte = if (offset > 0) (uint <<  offset.toInt).toByte else (uint >>> -offset.toInt).toByte
+    def >>>>(offset: Long): Byte = if (offset > 0) (uint >>> offset).toByte else (uint <<  -offset).toByte
+    def <<<<(offset: Long): Byte = if (offset > 0) (uint <<  offset).toByte else (uint >>> -offset).toByte
 
     def ||||(that: Byte): Byte = (self | that).toByte
   }
@@ -17,14 +17,16 @@ package object syntax {
 
     def uint: Int = 0xffff & self.toInt
 
-    def >>>>(offset: Long): Short = if (offset > 0) (uint >>> offset.toInt).toShort else (uint <<  -offset.toInt).toShort
-    def <<<<(offset: Long): Short = if (offset > 0) (uint <<  offset.toInt).toShort else (uint >>> -offset.toInt).toShort
+    def >>>>(offset: Long): Short = if (offset > 0) (uint >>> offset).toShort else (uint <<  -offset).toShort
+    def <<<<(offset: Long): Short = if (offset > 0) (uint <<  offset).toShort else (uint >>> -offset).toShort
 
     def ||||(that: Short): Short = (self | that).toShort
   }
 
   implicit class IntOps_Ghg39HP(val self: Int) extends AnyVal {
     def hex: String = (0 until 32).reverse.map(i => (self >> i) & 0x1).mkString("")
+
+    def uint: Int = self
 
     def >>>>(offset: Long): Int = if (offset > 0) self >>> offset.toInt else self <<  -offset.toInt
     def <<<<(offset: Long): Int = if (offset > 0) self <<  offset.toInt else self >>> -offset.toInt
@@ -34,6 +36,8 @@ package object syntax {
 
   implicit class LongOps_Ghg39HP(val self: Long) extends AnyVal {
     def hex: String = (0L until 64L).reverse.map(i => (self >> i) & 0x1).mkString("")
+
+    def uint: Int = self.toInt
 
     def >>>>(offset: Long): Long = if (offset > 0) self >>> offset else self <<  -offset
     def <<<<(offset: Long): Long = if (offset > 0) self <<  offset else self >>> -offset
