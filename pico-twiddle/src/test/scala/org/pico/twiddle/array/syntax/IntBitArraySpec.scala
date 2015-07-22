136d4f6166a2975cdc88cde550dc63c07bc37cf9
John Ky <jky@arbor.net>
2015-07-22 23:05:18 +1000

    Add missing property test.

--- pico-twiddle/src/test/scala/org/pico/twiddle/array/syntax/IntBitArraySpec.scala
+++ pico-twiddle/src/test/scala/org/pico/twiddle/array/syntax/IntBitArraySpec.scala
@@ -15,6 +15,16 @@ class IntBitArraySpec extends Specification with ScalaCheck {
     }.setGen2(Gen.choose(0L, 127L))
   }
 
+  "Shorts that are set can be retrieved again" in {
+    prop { (v: Short, offset: Long, wallpaper: Int) =>
+      val buffer = Array.fill(65)(wallpaper)
+      offset must be_>=(0L)
+      offset must be_<(128L)
+      buffer.int(offset, v)
+      buffer.int(offset) ==== v
+    }.setGen2(Gen.choose(0L, 127L))
+  }
+
   "Ints that are set can be retrieved again" in {
     prop { (v: Int, offset: Long, wallpaper: Int) =>
       val buffer = Array.fill(65)(wallpaper)
