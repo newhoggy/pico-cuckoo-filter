65f447780adb1f1815ba9432c17b4a4a732435ab
John Ky <jky@arbor.net>
2015-07-23 08:17:08 +1000

    Consecutive assignment test.

--- pico-twiddle/src/test/scala/org/pico/twiddle/array/syntax/ByteBitArraySpec.scala
+++ pico-twiddle/src/test/scala/org/pico/twiddle/array/syntax/ByteBitArraySpec.scala
@@ -44,4 +44,60 @@ class ByteBitArraySpec extends Specification with ScalaCheck {
       buffer.long(offset) ==== v
     }.setGen2(Gen.choose(0L, 127L))
   }
+
+  "Consecutive Bytes that are set can be retrieved again" in {
+    prop { (wallpaper: Byte, offset: Long, u: Byte, v: Byte, w: Byte) =>
+      val buffer = Array.fill(130)(wallpaper)
+      offset must be_>=(0L)
+      offset must be_<(128L)
+      buffer.byte(offset + 8 * 0, u)
+      buffer.byte(offset + 8 * 2, w)
+      buffer.byte(offset + 8 * 1, v)
+      buffer.byte(offset + 8 * 0) ==== u
+      buffer.byte(offset + 8 * 2) ==== w
+      buffer.byte(offset + 8 * 1) ==== v
+    }.setGen2(Gen.choose(0L, 127L))
+  }
+
+  "Consecutive Shorts that are set can be retrieved again" in {
+    prop { (wallpaper: Byte, offset: Long, u: Short, v: Short, w: Short) =>
+      val buffer = Array.fill(130)(wallpaper)
+      offset must be_>=(0L)
+      offset must be_<(128L)
+      buffer.short(offset + 8 * 0, u)
+      buffer.short(offset + 8 * 2, w)
+      buffer.short(offset + 8 * 1, v)
+      buffer.short(offset + 8 * 0) ==== u
+      buffer.short(offset + 8 * 2) ==== w
+      buffer.short(offset + 8 * 1) ==== v
+    }.setGen2(Gen.choose(0L, 127L))
+  }
+
+  "Consecutive Ints that are set can be retrieved again" in {
+    prop { (wallpaper: Byte, offset: Long, u: Int, v: Int, w: Int) =>
+      val buffer = Array.fill(130)(wallpaper)
+      offset must be_>=(0L)
+      offset must be_<(128L)
+      buffer.int(offset + 8 * 0, u)
+      buffer.int(offset + 8 * 2, w)
+      buffer.int(offset + 8 * 1, v)
+      buffer.int(offset + 8 * 0) ==== u
+      buffer.int(offset + 8 * 2) ==== w
+      buffer.int(offset + 8 * 1) ==== v
+    }.setGen2(Gen.choose(0L, 127L))
+  }
+
+  "Consecutive Longs that are set can be retrieved again" in {
+    prop { (wallpaper: Byte, offset: Long, u: Long, v: Long, w: Long) =>
+      val buffer = Array.fill(130)(wallpaper)
+      offset must be_>=(0L)
+      offset must be_<(128L)
+      buffer.long(offset + 8 * 0, u)
+      buffer.long(offset + 8 * 2, w)
+      buffer.long(offset + 8 * 1, v)
+      buffer.long(offset + 8 * 0) ==== u
+      buffer.long(offset + 8 * 2) ==== w
+      buffer.long(offset + 8 * 1) ==== v
+    }.setGen2(Gen.choose(0L, 127L))
+  }
 }
