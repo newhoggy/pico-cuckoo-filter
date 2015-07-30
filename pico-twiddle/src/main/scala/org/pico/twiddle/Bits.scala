33a78950436840ad90e2aaba320f2abde039ff8a
John Ky <jky@arbor.net>
2015-07-30 23:06:51 +1000

    New operations on Bits.

--- pico-twiddle/src/main/scala/org/pico/twiddle/Bits.scala
+++ pico-twiddle/src/main/scala/org/pico/twiddle/Bits.scala
@@ -11,4 +11,15 @@ case class Bits(value: Long) extends AnyVal {
   def >(that: Bits): Boolean = this.value > that.value
   def <=(that: Bits): Boolean = this.value <= that.value
   def >=(that: Bits): Boolean = this.value >= that.value
+
+  def min(that: Bits): Bits = Bits(this.value min that.value)
+  def max(that: Bits): Bits = Bits(this.value max that.value)
+
+  def *(that: Long)(implicit ev: DummyImplicit): Bits = Bits(this.value * that)
+  def /(that: Long)(implicit ev: DummyImplicit): Bits = Bits(this.value / that)
+  def %(that: Long)(implicit ev: DummyImplicit): Bits = Bits(this.value % that)
+
+  def byteIndexCeiling: Int = ((value + 7) / 8).toInt
+
+  def until(that: Bits) = (this.value until that.value).map(Bits)
 }
